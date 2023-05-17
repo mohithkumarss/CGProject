6e3429b09206b5649b6a5998682a591b8f27ee46
LeokondaMohith <109334910+mohithkumarss@users.noreply.github.com>
2023-05-17 19:21:21 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import leoRoutes from './routes/leo.routes.js';
+import leoRoutes from "./routes/leo.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limig: "50mb" }))
+app.use(express.json({ limig: "50mb" }));
 
-app.use("/api/v1/dalle", leoRoutes);
+app.use("/api/v1/leo", leoRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from Leokonda" })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from Leokonda" });
+});
 
-app.listen(8080, () => console.log('Server has started on port 8080'))
+app.listen(8080, () => console.log("Server has started on port 8080"));
