b5802630ed56c10129f7bb1e5cd29bbae6044833
LeokondaMohith <109334910+mohithkumarss@users.noreply.github.com>
2023-05-17 16:48:26 +0530

    now

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import dalleRoutes from './routes/dalle.routes.js';
+import leoRoutes from './routes/leo.routes.js';
 
 dotenv.config();
 
@@ -10,10 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limig: "50mb" }))
 
-app.use("/api/v1/dalle", dalleRoutes);
+app.use("/api/v1/dalle", leoRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E" })
+  res.status(200).json({ message: "Hello from Leokonda" })
 })
 
 app.listen(8080, () => console.log('Server has started on port 8080'))
